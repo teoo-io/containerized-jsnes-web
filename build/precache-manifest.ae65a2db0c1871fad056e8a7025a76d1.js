217933005312369aafc0b343fe6bee36747ab81d
Mateo Ortegon <m@teoo.io>
2021-12-26 02:39:01 -0800

    Added Super Mario and Bomberman 2 ROMs.

---
+++ build/precache-manifest.ae65a2db0c1871fad056e8a7025a76d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a22ff2456bd6ec7c21cbf8a1f499ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b3b3390b0e7ed1b294",
+    "url": "/static/css/main.f8d09ec5.chunk.css"
+  },
+  {
+    "revision": "07ce6498b0dab9db096e",
+    "url": "/static/js/2.fa5df9e5.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.fa5df9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b3b3390b0e7ed1b294",
+    "url": "/static/js/main.01dfd8e7.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
