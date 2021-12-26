ceb7323a8cb1d3a968cac87da1c964fadc040db2
Mateo Ortegon <m@teoo.io>
2021-12-26 02:05:20 -0800

    Added Super Mario and Bomberman 2 ROMs.

--- src/config.js
+++ src/config.js
@@ -5,12 +5,12 @@ const config = {
       super_mario_bros: {
           name: "Super Mario Bros",
           description: <span>An online emulation of Super Mario Bros NES ROM.</span>,
-          url: "http://localhost:3000/roms/Super Mario Bros (E).nes"
+          url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Super%20Mario%20Bros%20(E).nes"
       },
       bomberman_2: {
           name: "Bomberman 2",
           description: <span>An online emulation of Super Mario Bros NES ROM.</span>,
-          url: "http://localhost:3000/roms/Bomber_man2.nes"
+          url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Bomber_man2.nes"
       },
   },
   GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
