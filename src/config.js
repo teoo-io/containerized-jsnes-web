440affd9cf56f37688bf4c5e3241ff861fd94b8d
Mateo Ortegon <m@teoo.io>
2021-12-31 01:32:59 -0800

    Added ROMs.

--- src/config.js
+++ src/config.js
@@ -4,12 +4,22 @@ const config = {
   ROMS: {
       super_mario_bros: {
           name: "Super Mario Bros",
-          description: <span>An online emulation of Super Mario Bros NES ROM.</span>,
+          description: <span>Super Mario Bros ROM.</span>,
           url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Super%20Mario%20Bros%20(E).nes"
       },
       bomberman_2: {
           name: "Bomberman 2",
-          description: <span>An online emulation of Super Mario Bros NES ROM.</span>,
+          description: <span>Bomber Man 2 ROM.</span>,
+          url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Bomber_man2.nes"
+      },
+      super_mario_bros_3: {
+          name: "Super Mario Bros 3",
+          description: <span>Super Mario Bros 3 ROM.</span>,
+          url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Super%20Mario%20Bros%20(E).nes"
+      },
+      adventure_island: {
+          name: "Hudson's Adventure Island",
+          description: <span>Hudson's Adventure Island ROM.</span>,
           url: "https://cdn.jsdelivr.net/gh/blacksheep-git/jsnes-web@master/public/roms/Bomber_man2.nes"
       },
   },
